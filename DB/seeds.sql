0c23df359aab7f064b17915074b6a9507600be05
James Antley <jimmy.antley@gmail.com>
2021-06-22 01:31:00 -0400

    completed seeds

---
+++ DB/seeds.sql
@@ -0,0 +1,22 @@
+USE employee_DB;
+
+INSERT INTO department (dept_name)
+VALUES ("Forensics"), ("Engineering"), ("Legal"), ("Investigations");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ('DNA Analyst', 100000, 1), ('Toxicologist', 95000, 1),
+('Lead Engineer', 150000, 2), ('Software Engineer', 90000, 2),
+('Chief Legal Officer', 200000, 3), ('Lawyer', 100000, 3),
+('Special Agent', 175000, 4), ('Junior Fraud Investigator', 40000, 4); 
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ('Big', 'Boss', 1, 1),
+        ('Solid', 'Snake', 2, 1),
+        ('Loki', 'Laufeyson', 4, NULL),
+        ('Thomas', 'Tankengine', 2, 3),
+        ('Victor', 'Von Doom', 4, NULL),
+        ('Johnny', 'Bravo', 3, 4),
+        ('Finn', 'the Human', 3, NULL),
+        ('Jake', 'the Dog', 3, NULL),
+        ('Rick', 'Sanchez', 4, 2),
+        ('Morty', 'Smith', 1, 2);
--- connection.js
+++ connection.js
@@ -19,7 +19,7 @@ connection.connect((err) => {
     initQuestion();
 });
 
-///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 
 // Initial prompt
 const initQuestion = () => {
@@ -37,24 +37,12 @@ const initQuestion = () => {
             value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
             },
             {
-            name: "View All Employees By Manager",
-            value: "VIEW_EMPLOYEES_BY_MANAGER"
-            },
-            {
             name: "Add Employee",
             value: "ADD_EMPLOYEE"
             },
             {
-            name: "Remove Employee",
-            value: "REMOVE_EMPLOYEE"
-            },
-            {
             name: "Update Employee Role",
             value: "UPDATE_EMPLOYEE_ROLE"
-            },
-            {
-            name: "Update Employee Manager",
-            value: "UPDATE_EMPLOYEE_MANAGER"
             },
             {
             name: "View All Roles",
@@ -65,10 +53,6 @@ const initQuestion = () => {
             value: "ADD_ROLE"
             },
             {
-            name: "Remove Role",
-            value: "REMOVE_ROLE"
-            },
-            {
             name: "View All Departments",
             value: "VIEW_DEPARTMENTS"
             },
@@ -77,13 +61,10 @@ const initQuestion = () => {
             value: "ADD_DEPARTMENT"
             },
             {
-            name: "Remove Department",
-            value: "REMOVE_DEPARTMENT"
+            name: "Quit",
+            value: "QUIT"
             },
-            // { // Extra Credit
-            //   name: "View All Employees By Department",
-            //   value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
-            // },
+
             // { // Extra Credit
             //   name: "View All Employees By Manager",
             //   value: "VIEW_EMPLOYEES_BY_MANAGER"
@@ -104,10 +85,31 @@ const initQuestion = () => {
             //   name: "Remove Department",
             //   value: "REMOVE_DEPARTMENT"
             // },
+
             {
             name: "Quit",
             value: "QUIT"
-            }
-        ]
-    }]);
+            },
+        ],
+
+    }])
+    
+    .then((answer) => {
+        switch (answer.initQuestion) {
+            case 'VIEW_EMPLOYEES':
+                allEmps();
+                break;
+        }
+    })
 }
+
+const allEmps = () => {
+    const query = 'SELECT employee.id, employee.first_name, employee.last_name, role.id, role.title, role.salary FROM employee INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id) ORDER BY employee.id;';
+    
+    connection.query(query, (err, res) => {
+      if (err) throw err;
+      console.log("VIEW_EMPLOYEES");
+      console.table(res);  
+      initQuestion();
+    })
+};
