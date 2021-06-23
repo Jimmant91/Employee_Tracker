a9f0e06b682db3b426c7d7ebb80919d77ad7f213
James Antley <jimmy.antley@gmail.com>
2021-06-23 01:32:11 -0400

    Add Employee completed

--- connection.js
+++ connection.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 const mysql = require('mysql');
+const util = require('util');
 
 // Connect to MySQL Database
 const connection = mysql.createConnection(
@@ -18,6 +19,8 @@ connection.connect((err) => {
     if (err) throw err;
     initQuestion();
 });
+
+const query = util.promisify(connection.query).bind(connection);
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -97,6 +100,10 @@ const initQuestion = () => {
                 allEmps();
                 break;
 
+            case 'ADD_EMPLOYEE':
+                addEmp();
+                break;
+
             case 'VIEW_DEPARTMENTS':
                 allDept();
                 break;
@@ -129,6 +136,50 @@ const allEmps = () => {
     })
 };
 
+const addEmp = async () => {
+    // Awaiting the role to be selected at end of the following prompts
+    const roles = await query(`SELECT * FROM role`);
+    const roleChoice = roles.map((choice) => ({
+        name: choice.title,
+        value: choice.id,
+    }));
+
+    const answer = await inquirer.prompt([
+        {
+            name: "first_name",
+            type: "input",
+            message: "What is the employee's first name?",
+        },
+        {
+            name: "last_name",
+            type: "input",
+            message: "What is the employee's last name?",
+        },
+        {
+            name: "manager_id",
+            type: "input",
+            message: "What is the employee's manager ID? (Leave blank in not applicable)",
+        },
+        {
+            name: "role_id",
+            type: "list",
+            message: "What is the employee's role?",
+            choices: roleChoice,
+        }
+    ]);
+
+    const empInfo = "INSERT INTO employee (first_name, last_name, manager_id, role_id) VALUES (?,?,?,?)";
+    let answers = [
+        answer.first_name,
+        answer.last_name,
+        answer.manager_id,
+        answer.role_id,
+    ]
+    const res = await query(empInfo, answers);
+    console.table(res);
+    initQuestion();
+};
+
 const allDept = () => {
     const query = "SELECT department.dept_name AS Department FROM department ORDER by department.id";
 
@@ -140,7 +191,7 @@ const allDept = () => {
     })
 };
 
-const allEmployDep = () => {
+const allEmpDept = () => {
     // Want to shorten this query as much as possible
     const query = "SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, role.title AS Title, dept_name AS Department, role.salary AS Salary, CONCAT(m.first_name,' ', m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id JOIN role ON e.role_id = role.id JOIN department ON department.id = role.department_id ORDER BY dept_name ASC";
     
@@ -148,6 +199,6 @@ const allEmployDep = () => {
         if (err) throw err;
         console.log("VIEW_EMPLOYEES_BY_DEPARTMENT");
         console.table(res);  
-        start();
+        initQuestion();
     })
 };
