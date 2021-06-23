2e383ff1ad61d9185d4077fa1b5fd0ee9fbfb859
James Antley <jimmy.antley@gmail.com>
2021-06-23 03:58:24 -0400

    completed most of extra credit

--- connection.js
+++ connection.js
@@ -42,6 +42,10 @@ const initQuestion = () => {
                 value: "ADD_EMPLOYEE"
             },
             {
+                name: "Remove Employee",
+                value: "REMOVE_EMPLOYEE"
+            },
+            {
                 name: "View All Departments",
                 value: "VIEW_DEPARTMENTS"
             },
@@ -66,26 +70,25 @@ const initQuestion = () => {
                 value: "ADD_ROLE"
             },
             {
+                name: "Remove Role",
+                value: "REMOVE_ROLE"
+            },
+            {
                 name: "Update Employee Role",
                 value: "UPDATE_EMPLOYEE_ROLE"
             },
-            
-            // { // Extra Credit
-            //   name: "View All Employees By Manager",
-            //   value: "VIEW_EMPLOYEES_BY_MANAGER"
-            // },
-            // { // Extra Credit
-            //   name: "Remove Employee",
-            //   value: "REMOVE_EMPLOYEE"
-            // },
-            // { // Extra Credit
-            //   name: "Update Employee Manager",
-            //   value: "UPDATE_EMPLOYEE_MANAGER"
-            // },
-            // { //Extra Credit
-            //   name: "Remove Role",
-            //   value: "REMOVE_ROLE"
-            // },
+            {
+               name: "View All Employees By Manager",
+               value: "VIEW_EMPLOYEES_BY_MANAGER"
+            },
+            {
+                name: "Update Employee Manager",
+                value: "UPDATE_EMPLOYEE_MANAGER"
+            },
+            {
+                name: "See The Utilized Budget",
+                value: "UTILIZED BUDGET"
+            },
             {
             name: "Quit",
             value: "QUIT"
@@ -102,6 +105,10 @@ const initQuestion = () => {
 
             case 'ADD_EMPLOYEE':
                 addEmp();
+                break;
+
+            case 'REMOVE_EMPLOYEE':
+                removeEmp();
                 break;
 
             case 'VIEW_DEPARTMENTS':
@@ -132,6 +139,22 @@ const initQuestion = () => {
                 updateRole();
                 break;
 
+            case 'REMOVE_ROLE':
+                removeRole();
+                break;
+
+            case 'VIEW_EMPLOYEES_BY_MANAGER':
+                allEmpsMan();
+                break;
+
+            case 'UPDATE_EMPLOYEE_MANAGER':
+                updateEmpMan();
+                break;
+
+            case 'UTILIZED_BUDGET':
+                budget();
+                break;
+
             case 'QUIT':
                 connection.end();
                 break;
@@ -144,6 +167,16 @@ const initQuestion = () => {
 }
 
 // Calling all functions for my switch
+// Global delete function
+const deleteFromTable = async (table, idColumn, idToDelete) => {
+    const params = [table, idColumn, idToDelete];
+    const res = await query(`DELETE FROM ?? WHERE ?? = ?`, params);
+    // console.log(params);
+    console.table(res);
+    initQuestion();
+  };
+  
+// Display all employees (by id)
 const allEmps = () => {
     // Want to shorten this query as much as possible
     const query = "SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, role.title AS Title, dept_name AS Department, role.salary AS Salary, CONCAT(m.first_name,' ', m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id JOIN role ON e.role_id = role.id JOIN department ON department.id = role.department_id ORDER BY e.id";
@@ -156,6 +189,7 @@ const allEmps = () => {
     })
 };
 
+// Add employee
 const addEmp = async () => {
     // Awaiting the role to be selected at end of the following prompts
     const roles = await query(`SELECT * FROM role`);
@@ -200,6 +234,27 @@ const addEmp = async () => {
     initQuestion();
 };
 
+// Remove Employee
+const removeEmp = async () => {
+    const emps = await query(`SELECT * FROM employee`);
+    const empChoice = emps.map((choice) => ({
+      name: `${choice.first_name} ${choice.last_name}`,
+      value: choice.id,
+    }));
+  
+    const answer = await inquirer.prompt([
+        {
+            name: "employee_id",
+            type: "list",
+            message: "Which employee would you like to delete?",
+            choices: empChoice,
+        },
+    ]);
+  
+    deleteFromTable("employee", "id", answer.employee_id);
+};
+
+// Display all departments
 const allDept = () => {
     const query = "SELECT department.dept_name AS Department FROM department ORDER by department.id";
 
@@ -211,6 +266,7 @@ const allDept = () => {
     })
 };
 
+// Display all employees organized by department
 const allEmpDept = () => {
     // Want to shorten this query as much as possible
     const query = "SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, role.title AS Title, dept_name AS Department, role.salary AS Salary, CONCAT(m.first_name,' ', m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id JOIN role ON e.role_id = role.id JOIN department ON department.id = role.department_id ORDER BY dept_name ASC";
@@ -223,6 +279,7 @@ const allEmpDept = () => {
     })
 };
 
+// Add a department
 const addDept = async () => {
     const answer = await inquirer.prompt([
         {
@@ -239,15 +296,7 @@ const addDept = async () => {
     initQuestion();
 }
 
-// Global delete function
-const deleteFromTable = async (table, idColumn, idToDelete) => {
-    const params = [table, idColumn, idToDelete];
-    const res = await query(`DELETE FROM ?? WHERE ?? = ?`, params);
-    // console.log(params);
-    console.table(res);
-    initQuestion();
-  };
-
+// Remove a department
 const removeDept = async () => {
     const departments = await query(`SELECT * FROM department`);
     const departmentChoice = departments.map((choice) => ({
@@ -267,6 +316,7 @@ const removeDept = async () => {
     deleteFromTable("department", "id", answer.department_id);
 };
 
+// Display all employees organized by role
 const allRoles = () => {
     const query = "SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, role.title AS Title, dept_name AS Department, role.salary AS Salary, CONCAT(m.first_name,' ', m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id JOIN role ON e.role_id = role.id JOIN department ON department.id = role.department_id ORDER BY role.title ASC";
     
@@ -278,6 +328,7 @@ const allRoles = () => {
     });
 };
 
+//Add a role
 const addRole = async () => {
     const depts = await query('SELECT * FROM department;');
     const deptChoice = depts.map((choice) => ({
@@ -315,6 +366,27 @@ const addRole = async () => {
     initQuestion();
 }
 
+// Remove a role
+const removeRole = async () => {
+    const roles = await query(`SELECT * FROM role`);
+    const roleChoice = roles.map((choice) => ({
+      name: choice.title,
+      value: choice.id,
+    }));
+  
+    const answer = await inquirer.prompt([
+        {
+            name: "role_id",
+            type: "list",
+            message: "Which role would you like to delete?",
+            choices: roleChoice,
+        },
+    ]);
+  
+    deleteFromTable("role", "id", answer.role_id);
+};
+
+// Update an existing role
 const updateRole = async () => {
     const roles = await query("SELECT * FROM role;");
     const roleChoice = roles.map((choice) => ({
@@ -340,6 +412,51 @@ const updateRole = async () => {
     const answers = [answer.title, answer.role_id];
 
     const res = await query(roleInfo, answers);
+    console.table(res);
+    initQuestion();
+};
+
+// Display all employees organized by their Manager
+const allEmpsMan = () => {
+    const query = "SELECT e.id, CONCAT(e.first_name, ' ', e.last_name) AS Employee, role.title AS Title, dept_name AS Department, role.salary AS Salary, CONCAT(m.first_name, ' ', m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id JOIN role ON e.role_id = role.id JOIN department ON department.id = role.department_id ORDER BY e.manager_id";
+    
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.log("VIEW_EMPLOYEES_BY_MANAGER");
+        console.table(res);  
+        initQuestion();
+    })
+}
+
+// Update an employee's Manager
+const updateEmpMan = async () => {
+    const emps = await query(`SELECT * FROM employee`);
+    const empChoice = emps.map((choice) => ({
+      name: `${choice.first_name} ${choice.last_name}`,
+      value: choice.id,
+    }));
+  
+    const answer = await inquirer.prompt([
+        {
+            name: "manager_id",
+            type: "list",
+            message: "Select an employee you'd like to be the Manager:",
+            choices: empChoice,
+        },
+        {
+            name:"employee_id",
+            type: "list",
+            message: "Select an employee who needs a new Manager:",
+            choices: empChoice,
+        }
+    ]);
+
+    const empInfo = "UPDATE employee SET employee.manager_id=? WHERE employee.id=?;";
+    let answers = [
+        answer.manager_id,
+        answer.employee_id,
+    ]
+    const res = await query(empInfo, answers);
     console.table(res);
     initQuestion();
 };
